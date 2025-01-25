ca6f7b41c8d25c0d1ab3c638eb05d0b4a54319f6
kshitij <kshitijkokate97@gmail.com>
2025-01-25 15:48:00 +0530

    oops intro

---
+++ oops1.py
@@ -0,0 +1,17 @@
+class employee:
+    #function
+    def __init__(self):
+        self.id=123
+        self.salary=50000
+        self.designation="SDE"
+    
+    def travel(self,destination):
+        print(f"employe is travelling to {destination}")
+
+
+
+sam =employee()
+
+print(sam.salary)
+
+sam.travel("sangli")
