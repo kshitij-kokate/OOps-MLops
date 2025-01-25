e0fc08138c4270bf9676fc172ae9caa5dba46b16
kshitij <kshitijkokate97@gmail.com>
2025-01-25 16:34:03 +0530

    minor change

--- oops1.py
+++ oops1.py
@@ -1,17 +1,21 @@
-class employee:
-    #function
-    def __init__(self):
-        self.id=123
-        self.salary=50000
-        self.designation="SDE"
+# class employee:
+#     #function
+#     def __init__(self):
+#         self.id=123
+#         self.salary=50000
+#         self.designation="SDE"
     
-    def travel(self,destination):
-        print(f"employe is travelling to {destination}")
+#     def travel(self,destination):
+#         print(f"employe is travelling to {destination}")
 
 
 
-sam =employee()
+# sam =employee()
 
-print(sam.salary)
+# print(sam.salary)
 
-sam.travel("sangli")
+# sam.travel("sangli")
+
+from project1 import chatbook
+
+user1 = chatbook()
